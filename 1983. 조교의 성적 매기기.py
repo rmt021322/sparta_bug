f911ec8049be06774a13d23a8e507d88fb9add90
rmt021322 <rmt021322@naver.com>
2025-03-05 21:51:28 +0900

    second 1983. 조교의 성적 매기기.py

--- 1983. 조교의 성적 매기기.py
+++ 1983. 조교의 성적 매기기.py
@@ -1,16 +1,18 @@
 
 T = int(input())
-N, K = map(int, input().split()) #학생수 N, 학생 번호 K
 # 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
-score_list = ["A+", "A0",  "A-", "B+", "B0", "B-", "C+", "C0", "C-", "D0"] #평점리스트
-total_list = [ ]
-#print(score_list)
+
 for test_case in range(1, T + 1):
-    S, O, R = map(int, input().split())# 학생별 중간 기말 과제 점수_S중간 O기말 R과제
-    #print(S)
-    #중간 35 기말 45 과제 20 백분율
-    personal_score = S*0.35+O*0.45+R*0.2
-   # print(personal_score)
-    total_list = total_list + [personal_score] #리스트에 값 추가
-    print(total_list) #[74.6, 92.55000000000001]
+    N, K = map(int, input().split()) #학생수 N, 학생 번호 K
+    score_list = ["A+", "A0",  "A-", "B+", "B0", "B-", "C+", "C0", "C-", "D0"] #리스트에 평점 저장
+    #print(score_list)
+    total_list = [ ] #총점을 저장할 리스트
+    for i in range(N):
+        S, O, R = map(int, input().split())# 학생별 중간 기말 과제 점수_S중간 O기말 R과제
+       #print(S)
+        #중간 35 기말 45 과제 20 백분율
+        personal_score = S*0.35+O*0.45+R*0.2
+       # print(personal_score)
+        total_list = total_list + [personal_score] #총점 리스트에 백분율값 추가
+        
     

53f5ef2d5567f9e616929581b9830163759d8690
rmt021322 <rmt021322@naver.com>
2025-03-05 23:56:41 +0900

    fifth 1983. 조교의 성적 매기기.py

--- 1983. 조교의 성적 매기기.py
+++ 1983. 조교의 성적 매기기.py
@@ -28,41 +28,40 @@ for test_case in range(1, T + 1):
 
 
     #K값의 점수가 상위 10%이면 A+주고 90%면 D0 평점 부여하는 조건 만들기
-for rank in range(1, N+1):
-    #점수 기준 : rank값 순위
-    final_rank = 0
+for rank in range(1, N + 1):
+    # rank 값을 기준으로 성적 부여
+    final_rank = "" #""에 값 추가하기
+    
     #rank/10해서 0.n이면 A+
-        if rank/10 < 1 : #오류
-            final_rank = score_list[0]
-        #1.n이면 a0
-        elif 1<=rank/10 < 2:
-            final_rank = score_list[1]
-         #2.n이면 a-
-        elif 2<=rank/10 < 3:
-            final_rank = score_list[2]
-         #3.n이면 B+
-        elif 3<=rank/10 < 4:
-            final_rank = score_list[3]
-         #4.n이면 B0
-        elif 4<=rank/10 < 5:
-            final_rank = score_list[4]
-         #5.n이면 B-
-        elif 5<=rank/10 < 6:
-            final_rank = score_list[5]
-         #6.n이면 C+
-        elif 6<=rank/10 < 7:
-            final_rank = score_list[6]
-         #7.n이면 C0
-        elif 7<=rank/10 < 8:
-            final_rank = score_list[7]
-         #8.n이면 C-
-        elif 8<=rank/10 < 9:
-            final_rank = score_list[8]
-        #9.n이면 D0
-        else :
-            final_rank = score_list[9]
+    if rank / N < 0.1:
+        final_rank = score_list[0]  # A+
+    #0.1n이면 A0
+    elif 0.1 <= rank / N < 0.2:
+        final_rank = score_list[1]  # A0
+    #0.2n이면 A-
+    elif 0.2 <= rank / N < 0.3:
+        final_rank = score_list[2]  # A-
+    #0.3n이면 B+
+    elif 0.3 <= rank / N < 0.4:
+        final_rank = score_list[3]  # B+
+    #0.4n이면 B0
+    elif 0.4 <= rank / N < 0.5:
+        final_rank = score_list[4]  # B0
+    #0.5n이면 B-
+    elif 0.5 <= rank / N < 0.6:
+        final_rank = score_list[5]  # B-
+    #0.6n이면 C+
+    elif 0.6 <= rank / N < 0.7:
+        final_rank = score_list[6]  # C+
+    #0.7n이면 C0
+    elif 0.7 <= rank / N < 0.8:
+        final_rank = score_list[7]  # C0
+    #0.8n이면 C-
+    elif 0.8 <= rank / N < 0.9:
+        final_rank = score_list[8]  # C-
+    else : #0.9n이면 D0
+        final_rank = score_list[9]  # D0
 
-
-print(f'#{test_case} {final_rank}')
+        print(f'#{test_case} {final_rank}') #10 D0*N
     
 

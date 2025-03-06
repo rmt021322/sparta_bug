09dcfe75c249b9ca08fadf91cccf6a8c506bde9e
rmt021322 <rmt021322@naver.com>
2025-03-06 16:25:56 +0900

    first 1959.두 개의 숫자열.py

--- 0305/1959.두 개의 숫자열.py
+++ 0305/1959.두 개의 숫자열.py
@@ -1,1 +1,15 @@
 
+T = int(input())
+# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+for test_case in range(1, T + 1):
+    N, M = map(int, input().split())
+    Ai = list(map(int, input().split()))
+    Bj = list(map(int,input().split()))
+    
+    # 둘 중에 길이 긴 값을 Ai, 짧은 값을 Bj로 위치 변경
+
+    #Ai 길이 맞춰서 새 배열 만들기
+    
+    #새 배열 길이 내에서 Bi 위치 이동시키기
+    
+    #최댓값 찾는 반복문

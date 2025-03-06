197645e21b2dc54210d229d82c16f5ccc69cda92
rmt021322 <rmt021322@naver.com>
2025-03-06 14:27:41 +0900

    ☆ten★1983. 조교의 성적 매기기.py

--- 0305/1983. 조교의 성적 매기기.py
+++ 0305/1983. 조교의 성적 매기기.py
@@ -4,44 +4,44 @@
 T = int(input())  # 테스트 케이스 개수
 for test_case in range(1, T + 1):
     N, K = map(int, input().split()) #학생수 N, 학생 번호 K
-    score_list = ["A+", "A0",  "A-", "B+", "B0", "B-", "C+", "C0", "C-", "D0"] #리스트에 평점 저장
+    score_list = ["A+", "A0", "A-", "B+", "B0", "B-", "C+", "C0", "C-", "D0"] #리스트에 평점 저장
     total_list = [ ] #리스트에 총점 저장
 
-    for i in range(N): #전체학생수 N
+    for i in range(N): #전체 학생 수 N
         S, O, R = map(int, input().split())# 학생별 중간 기말 과제 점수_S중간 O기말 R과제
         #print(S)
         #중간 35 기말 45 과제 20 백분율
-        personal_score = S*0.35+O*0.45+R*0.2
+        personal_score = S*0.35+O*0.45+R*0.2 #개인 총점
         # print(personal_score)
-        total_list.append(personal_score)  # 리스트에 값 추가
+        total_list.append(personal_score)  # 학생 점수 리스트에 추가
         #print(total_list) 
         
-        #k번째 학생 점수 찾기
-        k_score = total_list[K - 1]  # K번 학생의 총점
-        #print(k_score) #하나 나와야 함
-        
-        
+      
+    #k번째 학생 점수 찾기
+    k_score = total_list[K - 1]  # K번 학생의 총점 k_score는 total_list K-1위치
+    #찾을 점수 k_score 인덱스 K-1
+    #print(k_score) #정렬 전 찾아놓기
+     
+
     # 성적을 내림차순으로 정렬
     #성적-평점 잇기
     #1. 총점리스트 줄세우기
     #버블정렬 내림차순
-    for i in range(N - 1):
+    for i in range(1, N):
         for j in range(N - 1 - i):
             if total_list[j] < total_list[j + 1]:  # 앞의 값이 작으면
                 total_list[j], total_list[j + 1] = total_list[j + 1], total_list[j]  # 위치 교환
             #print(total_list)  
+
+    #print(score_list)
+ 
+
+    # 총점 내림차순 이후 K번 학생의 점수에 해당하는 평점 구하기
+    k_rank = total_list.index(k_score) #정렬된 total_list에서 k_score의 위치 저장
+    grade = score_list[k_rank // (N // 10)]  # 10분위로 나누어 평점 결정
+
+    print(f'#{test_case} {grade}')
    
-    #k번째가 정렬후 리스트에서 몇번째인지 찾기
-    rank = total_list.index(k_score)
-    #print(rank) 
-    
-    #10%단위 
-    rank_division = N // 10  # 10% 단위로 나누기
-
-    #상위 10%부터 A+ A0 A- .... D0 부여하기
-    final_rank = score_list[rank // rank_division]  # 등급 계산
-
-    print(f'#{test_case} {final_rank}')
     '''
     #내꺼만들기#
     
@@ -51,12 +51,9 @@ for test_case in range(1, T + 1):
      #k번째가 정렬후 리스트에서 몇번째인지 찾기
     rank = total_list.index(k_score)
     #print(rank) 
+
+    #평점결정방법
+    grade = score_list[k_rank // (N // 10)]  # 10분위로 나누어 평점 결정
     
-    #10%단위 
-    rank_division = N // 10  # 10% 단위로 나누기
-
-    #상위 10%부터 A+ A0 A- .... D0 부여하기
-    final_rank = score_list[rank // rank_division]  # 등급 계산
-
-    print(f'#{test_case} {final_rank}')
+    
     '''

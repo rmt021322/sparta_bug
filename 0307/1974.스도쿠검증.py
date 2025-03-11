f58c14bb720faed079fb15253c8f22ad9b18c96c
rmt021322 <rmt021322@naver.com>
2025-03-11 17:39:26 +0900

    third 1974.스도쿠검증.py

--- 0307/1974.스도쿠검증.py
+++ 0307/1974.스도쿠검증.py
@@ -12,17 +12,37 @@ for test_case in range(1,T+1):
 
     number = [1,2,3,4,5,6,7,8,9] #확인할 숫자 값 리스트
 
-    #이동하면서 확인
-    for i in range(1, 10) :
-    #가로 세로 1부터 9까지 숫자 한번씩
-    
+    # 이동하면서 확인
+    # 가로 세로 1부터 9까지 숫자 한번씩
+    for i in range(N):
+        row_set = set()  # 가로확인
+        col_set = set()  # 세로 확인
+        # arr(i,j) 위치에서
+        for j in number:
+            row_set.add(arr[i][j])
+            col_set.add(arr[i][j])
 
-    # arr(i,j) 위치에서
-        # 가로로 확인
-        # 세로로 확인
-        #3*3 작은 격자에서 1부터 9까지 숫자 한번씩
+        if len(row_set) != N or len(col_set) != N:  # 길이가 N이 아니면
+            answer = 0  # 잘 되어있지 않으면 0 출력
 
-    #겹치는 숫자가 없으면 1 출력
-    #아니면 0 출력
+        # 3*3 작은 격자에서 1부터 9까지 숫자 한번씩
 
+        ''' #작은 3*3배열의 왼쪽 위에서 시작해서 삼각형 범위 값이 잘 되어 있나 확인
+        (0,0) (0,3), (0,6)
+        (3,0), (3,3), (3,6)
+        (6,0), (6,3), (6,6)
+        '''  # 격자에서 (i,j)의 위치들
+        for i in range(0, N, 3):
+            for j in range(0, N, 3):
+                box_set = set()
+                # (r,c): 작은 정사각형 내에서 순회하는 위치
+                for r in range(i, i + 3):
+                    for c in range(j, j + 3):
+                        box_set.add(arr[r][c])
 
+                if len(box_set) != N:
+                    answer = 0
+        print(f'#{test_case} {answer}')
+
+    # 겹치는 숫자가 없으면 1 출력
+    # 아니면 0 출력

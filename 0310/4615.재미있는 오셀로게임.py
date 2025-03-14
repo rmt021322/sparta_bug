ac73a27ae12da6dc2644eca0023778f2f09d226d
rmt021322 <rmt021322@naver.com>
2025-03-15 03:10:12 +0900

    second 4615.재미있는 오셀로게임.py

--- 0310/4615.재미있는 오셀로게임.py
+++ 0310/4615.재미있는 오셀로게임.py
@@ -1,3 +1,12 @@
+def change_color :
+    #가로, 세로, 대각선으로 놓을 돌과 놓인돌 사이
+    # 다른 색 돌을 내 색으로 바꾸기
+    
+    #흑->백
+    
+    #백->흑
+    
+    
 
 T = int(input())
 # 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
@@ -6,6 +15,9 @@ for test_case in range(1, T + 1):
     for i in range(M): #
         row, column, color = map(int,input().split()) #돌 놓을 위치 row, column과 돌 색 color
         #돌을 놓을 수 없는 곳은 입력으로 주어지지 않음
+        #흑돌 1 백돌 2
+        
+        
         
     #게임 종료 후 흑돌, 백돌 수 출력하기
     #print(f'#{test_case} {blackstone} {whitestone}')

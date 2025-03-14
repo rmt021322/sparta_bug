888d7591a7b1a99554eaabd2ff5d2449b62c08d4
rmt021322 <rmt021322@naver.com>
2025-03-15 02:27:11 +0900

    first 12004.구구단1.py

--- 0310/12004.구구단1.py
+++ 0310/12004.구구단1.py
@@ -1,1 +1,18 @@
 
+#import sys
+#sys.stdin = open("input.txt", "r")
+
+TC = int(input())
+# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+for test_case in range(1, T + 1):
+    N = int(input())
+    #주어진 정수 N이 1이상 9 이하의 두 수 a,b의 곱으로 표현될 수 있을까
+    if 1<= N <= 9 : #N이 범위 내일때
+        #a, b의 곱일 수 있는 경우
+       #있으면 Yes
+    
+       #없으면 No
+
+    
+    
+    print(f'#{test_case} {answer}')

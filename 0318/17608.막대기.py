59cbc6a23ccb50b23ed532a84cfe75f44b71495d
rmt021322 <rmt021322@naver.com>
2025-03-18 15:19:12 +0900

    second 17608.막대기.py

--- 0318/17608.막대기.py
+++ 0318/17608.막대기.py
@@ -1,7 +1,7 @@
 '''
     # stick 리스트 뒤집기_오른쪽에서 봤을 때 보이는 탑 개수
     # 리스트를 뒤집어서 왼쪽방향 탑이 오른쪽으로 오도록
-    sorted(stick, reverse=True)
+    sorted(stick, reverse=True)?
     '''
 
 N = int(input())
@@ -15,9 +15,9 @@ for h in range(N): #stick 리스트에 높이 저장: N이 5일 때 인덱스 01
 
     # print(stick)
 
-for i in range(N-1, -1, -1): #리스트 뒤집기 :N-1부터 0번인덱스까지 역순
-    if stick[i] > highest:
-        count += 1
-        highest = stick[i]
+for i in range(N-1, -1, -1): #리스트 뒤집기 :N-1부터 0번인덱스까지 역순으로
+    if stick[i] > highest: #i번째 stick값이 최댓값보다 크면
+        count += 1 #수량 증가하고
+        highest = stick[i] #값도 저장
 
 print(count)

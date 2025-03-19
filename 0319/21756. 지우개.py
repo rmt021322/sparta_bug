2c10c72be83dd5a11c10d837e20b2fdc7d798679
rmt021322 <rmt021322@naver.com>
2025-03-19 15:25:09 +0900

    third 21756. 지우개.py

--- 0319/21756. 지우개.py
+++ 0319/21756. 지우개.py
@@ -35,6 +35,8 @@ for a in range(1, N + 1):
     # 마지막 남은 숫자 하나 출력
 '''
 
+'''
+
 N = int(input())  # 입력된 값
 arr = [0]*(N+1)  # 0값이 N개인 배열 만들기
 new_arr = []
@@ -57,4 +59,29 @@ while len(new_arr) < 1 :
      # => 새 리스트 길이가 1이면 출력
     answer = len(new_arr)
     print(answer)
+'''
+
+N = int(input())  # 입력된 값
+arr = [0] * (N + 1)  # 0값이 N개인 배열 만들기
+new_arr = []
+for i in range(1, N + 1):  # 1부터 n+1칸까지 i값 저장
+    arr[i] = i
+
+# 정확히 하나 남을 때까지 아래 작업 반복
+while len(new_arr) < 1:
+    # 지우개로 홀수번 칸 수들을 모두 지움
+    # 남은 수를 왼쪽으로 모음
+    # => 리스트 새로 만들어서 남아야하는 원소만 추가
+    # => 짝수번 칸들만 모아서 새 리스트 만듦
+    for a in range(1, N + 1):
+        if a % 2 == 0:  # 인덱스 자릿수를 계산하는 방법
+            # b를 인덱스값으로 인식하고 계산하기
+            # 어떻게???
+            new_arr += arr[a]  # 이렇게?
+            #IndexError: list index out of range
+
+    # 위 과정을 반복해 숫자가 1개 남으면 중단
+    # => 새 리스트 길이가 1이면 출력
+answer = len(new_arr)
+print(answer)
 

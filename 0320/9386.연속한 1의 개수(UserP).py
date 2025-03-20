a7a826af56b7160aeafd33205bab71c1f536ba27
rmt021322 <rmt021322@naver.com>
2025-03-21 07:23:56 +0900

    first 9386.연속한 1의 개수(UserP).py

--- 0320/9386.연속한 1의 개수(UserP).py
+++ 0320/9386.연속한 1의 개수(UserP).py
@@ -1,1 +1,22 @@
+#연속한 1의 개수 중 최댓값
+#01011010111 => 3 출력
+T = int(input())
+for testcase in range(1,T+1) :
+    N = int(input())
+    arr = list(map(int,input()))
+    countone = 0
+    maxone = float('-inf')
 
+    for i in range(N):
+        if arr[i] == 1 :#1이 나오면
+            countone += 1 
+        
+        else : #arr[i] == 0
+            if maxone <= countone : #현재 1 개수가 최댓값보다 크면
+                maxone = countone #갱신
+                countone = 0 #초기화
+
+    if maxone <= countone:
+        maxone = countone
+
+    print(f'#{testcase} {maxone}')

e7ab692f31677a055b53169a447645d5b857cf47
rmt021322 <rmt021322@naver.com>
2025-03-21 16:34:00 +0900

    second 5185.이진수.py
    0더하기withGPT : 확실하게 내꺼하기

--- 0320/5185.이진수.py
+++ 0320/5185.이진수.py
@@ -25,21 +25,27 @@ for testcase in range(1,T+1):
         #뒤에서부터 한자리씩 가져와서 거듭제곱
         #16진수 = 마지막 자릿수가 16**0
         #1A3F = 1*(16**3) + A*(16**2) + 3*(16**1) + F*(16**0)
-        saveint += hexmap.index(nhex[-(i+1)]) * (16**i)
+        saveint += hexmap.index(nhex[-(i+1)]) * (16**i) #16진수를 통째로 10진수로 바꿈
     #print(saveint)
 
     #10to2 : 2로 나눠 나머지저장
     if saveint == 0:
         savebin = "0"
+        #savebin = "0000"도 됨
+        #마지막 앞자리 0 보장하기의 대상이 아니게 될 뿐임
     else :
         while saveint >0:
             savebin = str(savebin%2) + savebin #0101 이진수 만들기
             saveint //= 2 #몫 업데이트
 
     #앞자리 0 보장하기
+    #16진수 낱개를 2진수로 바꾼 게 아니라
+    #주어진 16진수 자체를 2진수로 바꿔서 16진수 맨 앞 0이 없다,
+    #=> 맨 앞 0 만들어주기
+    #조건제시
     lenbin= 4 * int(N)  #16진수 1자리 = 2진수 4자리
     while len(savebin) < lenbin :
-       savebin = "0" + savebin 
+       savebin = "0" + savebin #★
 
 
     print(f'#{testcase} {savebin}')
